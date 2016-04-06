b32e8104390e1d36f5895778ddacf35596490844
Trav <jorkilla@gmail.com>
2016-04-05 23:56:10 -1000

    Million Dollar App
    TSA APP

--- js/main.js
+++ js/main.js
@@ -15,129 +15,73 @@ var GameState = {
     },
 
     preload: function() {
-        this.load.image("eightBall", "assets/images/eightBallCoconut.png");
+        this.load.image("arrow", "assets/images/arrow-19-512.png");
     },
 
 
     create: function() {
-        this.coconutTextHeaderHeight = game.height / 2 - 155;
-        this.playButton = game.add.button(game.width / 2, game.height - 150, "eightBall", this.fadeOut, this);
-        this.playButton.anchor.set(0.5);
 
         var style = {
             font: "25px helvetica",
             fill: "#ffffff",
             align: "center"
         }
+        var ripped = "The 1.4 million dollar TSA APP\nTap the Arrow.";
+        var taxPayersGotOwned = game.add.text(game.width / 2, game.height / 2 - 150, ripped, style);
+        taxPayersGotOwned.anchor.setTo(0.5);
+        taxPayersGotOwned.stroke = '#000000';
+        taxPayersGotOwned.strokeThickness = 8;
 
-        var question = "MAGIC COCONUT Says Ask One Question";
-        var coconutText = game.add.text(game.width / 2, game.height / 2 - 100, question, style);
-        coconutText.anchor.setTo(0.5);
-        coconutText.stroke = '#000000';
-        coconutText.strokeThickness = 8;
+        //var money = predictions[game.rnd.between(0, 1)];
+        var money = Math.floor(Math.random() * 2);
+        var imRich;
 
-        //Earthquake effect
-        //we needto add margin tot he world, so the camera can move
-        var margin = 50;
-        //and set the world's bounds according to the given margin
-        var x = -margin;
-        var y = -margin;
-        var w = game.world.width + margin * 2;
-        var h = game.world.height + margin * 2;
-        //it's not necessary to increase height, we do it to keep uniformity
-        game.world.setBounds(x, y, w, h);
-        //we make sure camera is at position(0,0)
-        game.world.camera.position.set(0);
+
+        //million dollas right here.
+        this.millionDollaFunction(money);
+       
 
     },
 
 
-    fadeOut: function() {
-        if (this.playButton.input.enabled === true) {
-            this.playButton.input.enabled = false;
-            this.addQuake();
+     fadeOut: function() {
+        if (this.arrowButton.input.enabled === true) {
+            this.arrowButton.input.enabled = false;
 
-            var predictions = [
-            "NEVERMIND YOUR QUESTIONS, WE GO BEACH!",
-            "YES...I MEAN NO \n WAIT I MEAN... \nWHAT WAS DA QUESTION AGAIN?",
-            "YOU SURE YOU WANNA ASK DAT \n QUESTION TO ONE COCONUT?",
-            "WHAT KINE QUESTION IS DAT \n ASK ONE NUDDA ONE",
-            "Who cares?",
-            "COCONUT STAY HUNGRY \n CANNOT TINK RIGHT NOW TRY AGAIN",
-            "COCONUT STAY BROKEN \n CANNOT PREDICT NOW TRY AGAIN",
-            "CHEE-HEE \nMIGHT HAPPEN, MIGHT NOT WHO KNOWS. ",
-            "HOW SHOULD I KNOW \nI'M JUST ONE COCONUT",
-            "Why You ask That, \nI starting to tink you the coconut here...", 
-            "HO BRAH, why you even ask Dat\n I NOT GON TELL U!", 
-            "HALA Y U ASK DAT 4!", 
-            "YOU TALKING TO ONE COCONUT \n ...U MUST BE CRAZY.",
-            "DA ANSWER IS \n I DON'T KNOW.",
-            "THE GUY WHO MADE THIS APP \n TINKS HE'S FUNNY \n HE IS NOT. \n ASK ONE NUDDA QUESTION."
-            ];
+            this.arrowButton.alpha = 0.1;
+
            
-            var randomPredictions = predictions[game.rnd.between(0, predictions.length - 1)];
-            var style = {
-                font: "25px helvetica",
-                fill: "#ffffff",
-                align: "center"
-            }
-            this.randomText = game.add.text(game.width / 2, game.height / 2 + 100, randomPredictions, style);
-            this.randomText.anchor.set(0.5);
-            this.randomText.stroke = '#000000';
-            this.randomText.strokeThickness = 8;
             console.log("hello");
-            game.time.events.add(Phaser.Timer.SECOND * 4, function(randomText, playButton) {
-                this.randomText.destroy();
-                this.newButton();
+            game.time.events.add(Phaser.Timer.SECOND * 0.1, function(randomText, arrowButton) {
+                this.arrowButton.destroy();
+                var money = Math.floor(Math.random() * 2);
+                this.millionDollaFunction(money);
+                this.arrowButton.input.enable = true;
             }, this);
-
         }
     },
 
-    newButton: function() {
-        this.playButton.input.enable = true;
-        this.playButton = game.add.button(game.width / 2, game.height - 150, "eightBall", this.fadeOut, this);
-        this.playButton.anchor.set(0.5);
+    millionDollaFunction: function (showMeTheMoney) {
+       
+        // this.arrowButton = game.add.button(game.width / 2, game.height - 150, "arrow", this.arrowFunction, this);
+        // this.arrowButton.anchor.set(0.5);
+        imRich = (showMeTheMoney <1) ? this.rightArrow():this.leftArrow();
+    },
 
-        var style = {
-            font: "25px helvetica",
-            fill: "#ffffff",
-            align: "center"
-        }
-
-        var question = "MAGIC COCONUT Says Ask One Question";
-        var coconutText = game.add.text(game.width / 2, game.height / 2 - 100, question, style);
-        coconutText.anchor.setTo(0.5);
-        coconutText.stroke = '#000000';
-        coconutText.strokeThickness = 8;
+        rightArrow: function() {
+            this.arrowButton = game.add.button(game.width / 2, game.height - 150, "arrow", this.fadeOut, this);
+            this.arrowButton.anchor.set(0.5);
+            this.arrowButton.scale.setTo(-1, 0.5);
 
 
     },
 
-    addQuake: function() {
-        // define the camera offset for the quake
-        var rumbleOffset = 10;
-        // move according to the camera's current position
-        var properties = {
-            x: game.camera.x - rumbleOffset
-        };
-        // make it a relly fast movement
-        var duration = 100;
-        // because it will repeat
-        var repeat = 4;
-        // use bounce in-out to soften it a little bit
-        var ease = Phaser.Easing.Bounce.InOut;
-        var autoStart = false;
-        // a little delay because we will run it indefinitely
-        var delay = 0;
-        // go back to the original position
-        var yoyo = true;
-        var quake = game.add.tween(game.camera)
-            .to(properties, duration, ease, autoStart, delay, 4, yoyo);
-        // we're using this line for the example to run indefinitely
-        //quake.onComplete.addOnce(addQuake);
-        // let the earthquake begins
-        quake.start();
+        leftArrow: function() {
+            this.arrowButton = game.add.button(game.width / 2, game.height - 150, "arrow", this.fadeOut, this);
+            this.arrowButton.anchor.set(0.5);
+            this.arrowButton.scale.setTo(1, 0.5);
+
+
     },
 
 
