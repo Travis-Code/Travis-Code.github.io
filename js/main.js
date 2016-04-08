5ca8eeb18fd1f6f13373e9fa8a4a553e1c46874a
Trav <jorkilla@gmail.com>
2016-04-07 21:13:11 -1000

    RIC FLAIR WOO APP
    WOO!

--- js/main.js
+++ js/main.js
@@ -1,87 +1,84 @@
 //game will have only 1 state
 var GameState = {
-
     //initiate game settings
     init: function() {
         //adapt to screen size, fit all the game
         this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         this.scale.pageAlignHorizontally = true;
         this.scale.pageAlignVertically = true;
-        this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        this.game.physics.arcade.gravity.y = 1000
-        this.cursors = this.game.input.keyboard.createCursorKeys();
         this.game.world.setBounds(0, 0, 600, 375);
-
     },
 
     preload: function() {
-        this.load.image("arrow", "assets/images/arrow-19-512.png");
+        this.load.image("ricFlair", "assets/images/ricFlair.jpg");
+        this.game.load.audio("woo", ["assets/sounds/woo.mp3", "assets/sounds/woo.ogg"]);
     },
-
 
     create: function() {
+        this.woo();
+        
+        //Earthquake effect
+        //we needto add margin tot he world, so the camera can move
+        var margin = 50;
+        //and set the world's bounds according to the given margin
+        var x = -margin;
+        var y = -margin;
+        var w = game.world.width + margin * 2;
+        var h = game.world.height + margin * 2;
+        //it's not necessary to increase height, we do it to keep uniformity
+        game.world.setBounds(x, y, w, h);
+        //we make sure camera is at position(0,0)
+        game.world.camera.position.set(0);
 
-        var style = {
-            font: "25px helvetica",
-            fill: "#ffffff",
-            align: "center"
-        }
-        var ripped = "The 1.4 million dollar TSA APP\nTap the Arrow.";
-        var taxPayersGotOwned = game.add.text(game.width / 2, game.height / 2 - 150, ripped, style);
-        taxPayersGotOwned.anchor.setTo(0.5);
-        taxPayersGotOwned.stroke = '#000000';
-        taxPayersGotOwned.strokeThickness = 8;
-
-        //var money = predictions[game.rnd.between(0, 1)];
-        var money = Math.floor(Math.random() * 2);
-        var imRich;
-
-
-        //million dollas right here.
-        this.millionDollaFunction(money);
-       
 
     },
 
-
-     fadeOut: function() {
-        if (this.arrowButton.input.enabled === true) {
-            this.arrowButton.input.enabled = false;
-
-            this.arrowButton.alpha = 0.1;
-
-           
-            console.log("hello");
-            game.time.events.add(Phaser.Timer.SECOND * 0.1, function(randomText, arrowButton) {
-                this.arrowButton.destroy();
-                var money = Math.floor(Math.random() * 2);
-                this.millionDollaFunction(money);
-                this.arrowButton.input.enable = true;
+    fadeOut: function() {
+        if (this.wooButton.input.enabled === true) {
+            this.wooButton.input.enabled = false;
+            this.wooButton.alpha = 0.1;
+            var cheer = game.add.audio("woo");
+            this.addQuake();
+            cheer.play();
+            console.log("WOOOO");
+            game.time.events.add(Phaser.Timer.SECOND * 0.2, function(randomText, wooButton){
+                this.wooButton.destroy();
+                this.woo(cheer);
+                this.wooButton.input.enable = true;
             }, this);
         }
     },
 
-    millionDollaFunction: function (showMeTheMoney) {
-       
-        // this.arrowButton = game.add.button(game.width / 2, game.height - 150, "arrow", this.arrowFunction, this);
-        // this.arrowButton.anchor.set(0.5);
-        imRich = (showMeTheMoney <1) ? this.rightArrow():this.leftArrow();
+    woo: function() {
+        this.wooButton = game.add.button(game.width / 2, game.height/2+5, "ricFlair", this.fadeOut, this);
+        this.wooButton.anchor.set(0.5);
+        this.wooButton.scale.setTo(1, 0.98);
     },
 
-        rightArrow: function() {
-            this.arrowButton = game.add.button(game.width / 2, game.height - 150, "arrow", this.fadeOut, this);
-            this.arrowButton.anchor.set(0.5);
-            this.arrowButton.scale.setTo(-1, 0.5);
-
-
-    },
-
-        leftArrow: function() {
-            this.arrowButton = game.add.button(game.width / 2, game.height - 150, "arrow", this.fadeOut, this);
-            this.arrowButton.anchor.set(0.5);
-            this.arrowButton.scale.setTo(1, 0.5);
-
-
+    addQuake: function() {
+        // define the camera offset for the quake
+        var rumbleOffset = 10;
+        // move according to the camera's current position
+        var properties = {
+            x: game.camera.x - rumbleOffset
+        };
+        // make it a relly fast movement
+        var duration = 100;
+        // because it will repeat
+        var repeat = 4;
+        // use bounce in-out to soften it a little bit
+        var ease = Phaser.Easing.Bounce.InOut;
+        var autoStart = false;
+        // a little delay because we will run it indefinitely
+        var delay = 0;
+        // go back to the original position
+        var yoyo = true;
+        var quake = game.add.tween(game.camera)
+            .to(properties, duration, ease, autoStart, delay, 4, yoyo);
+        // we're using this line for the example to run indefinitely
+        //quake.onComplete.addOnce(addQuake);
+        // let the earthquake begins
+        quake.start();
     },
 
 
