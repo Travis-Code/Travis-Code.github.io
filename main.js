773d6b898e272b15d93de5ba93ee6051929dd229
Trav <jorkilla@gmail.com>
2016-04-25 16:51:57 -1000

    parkour kid
    demo level 1

---
+++ main.js
@@ -0,0 +1,20 @@
+
+var Parkour = Parkour || {};
+
+
+//Parkour.dimensions = Parkour.getGameLandscapeDimensions(700,414);
+
+//create a new Phaser.Game object and assign it to our custom Parkour.game property.
+// then add our states to the game property.
+Parkour.game = new Phaser.Game(700, 414, Phaser.AUTO);
+Parkour.game.state.add("Boot", Parkour.BootState);
+Parkour.game.state.add("Preload", Parkour.PreloadState);
+Parkour.game.state.add("TitleScreen", Parkour.titleScreenState);
+Parkour.game.state.add("Game", Parkour.GameState);
+Parkour.game.state.add("Win", Parkour.winScreenState);
+
+
+//start our boot state
+Parkour.game.state.start("Boot");
+
+
