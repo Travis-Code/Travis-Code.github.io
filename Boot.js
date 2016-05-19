e5434b6858f7c8d9455bfb764e18b8225a7cd784
Trav <jorkilla@gmail.com>
2016-05-18 21:53:54 -1000

    screen fix

--- Boot.js
+++ Boot.js
@@ -5,22 +5,18 @@ Parkour.BootState ={
 	init: function(){
 		this.game.stage.backgroundColor = "#fff";
 		// this.game.scaleMode = Phaser.ScaleManager.EXACT_FIT;
-		this.game.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		//this.game.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		// this.scale.pageAlignHorizontally = true;
 		// this.scale.pageAlignVertically = true;
-		//initialize the arcade physics system
 	    this.scale.pageAlignVertically = true;
 	    this.scale.pageAlignHorizontally = true;
 	    this.scale.setShowAll();
 	    this.scale.refresh();
-	
-
 
 		this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
 	},
 	preload: function (){
-		// assets we'll use in the loading screen.
 		this.load.image("loadingBar", "assets/sprites/loading.png");
 	},
 	create: function(){
